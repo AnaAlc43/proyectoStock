62ec0c4702caa0edc3d4801c85937e92c57b39ef
ana _alcaino <anaanituss@gmail.com>
2023-10-25 21:04:07 -0300

    Actualización 25/10/23

---
+++ Stock.COMMON/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stock.COMMON.Entities;
+using Stock.COMMON.Interfaces;
+
+namespace Stock.COMMON.Interfaces.Repositories
+{
+    public  interface IProductRepository : IUnitOfWork<Product>
+
+    {
+        Task<Product> GetById(int productId);
+
+        Task Update(Product product);
+        Task Delete(int actorId);
+        Task<List<Actor>> GetAllActors();
+    }
+}
