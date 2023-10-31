abe24038afdc9e2fe192154ea0b9807b482524ff
ana _alcaino <anaanituss@gmail.com>
2023-10-31 20:37:10 -0300

    DbContextActualización

--- Stock.BusinessRules/Wrappers/User/WDeleteUser.cs
+++ Stock.BusinessRules/Wrappers/User/WDeleteUser.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stock.BusinessRules.Wrappers;
 
 namespace Stock.BusinessRules.Wrappers.User
 {
-    public class WDeleteUser
+    public class WDeleteUser : WBase
     {
-
+        public int IdUser { get; set; }
     }
 }
